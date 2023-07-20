5e4458d65d6086ec4a5d20b904f375d230d95bad
Jan Bures <bures.work@gmail.com>
2023-07-20 09:57:16 +0200

    update analysis part page

--- dash_app/pages/analyze_propaganda.py
+++ dash_app/pages/analyze_propaganda.py
@@ -33,8 +33,8 @@ from dash_app.ui_components import (
 
 dash.register_page(__name__, path="/analyse")
 
-# TODO / NOTE: so far the only example of cached data, this is the data Christian provided, but it is manually edited so
-#  it only contains a few highlighted sentences...
+# TODO / NOTE: so far this is the only example of cached data, this is the data Christian provided, but it is manually
+#  edited so it only contains a few highlighted sentences...
 # TODO: change this to work with dictionaries, NOT PANDAS DATAFRAME!! - my mistake...
 df = pd.read_csv("data_chris_edited.csv")
 
@@ -55,6 +55,7 @@ layout = [
 @callback(
     Output(component_id="analysis_results-container", component_property="children"),
     Output(component_id="container-all_analysis", component_property="style"),
+    Output(component_id="footer", component_property="fixed"),
     Output(component_id="call-to-action", component_property="style"),
     Input(component_id="button-try-example", component_property="n_clicks"),
     prevent_initial_call=True,  # this prevents callback triggering at page load (before the Submit button is clicked)
@@ -79,38 +80,46 @@ def process_example(n_clicks):
         "visibility": "visible",
         "transition": "opacity 1.0s ease",
         "min-height": 400,
-    }, call_to_action_style
+    }, False, call_to_action_style
 
 
-@callback(
-    Output(component_id="analysis_results-container", component_property="children"),
-    Output(component_id="container-all_analysis", component_property="style"),
-    Output(component_id="call-to-action", component_property="style"),
-    Input(component_id="button-submit-original", component_property="n_clicks"),
-    State(component_id="input-text_to_analyze", component_property="value"),
-    prevent_initial_call=True,  # this prevents callback triggering at page load (before the Submit button is clicked)
-    suppress_callback_exceptions=True,
-)
-def process_original(n_clicks, input_text):
-    call_to_action_style = {"opacity": 0, "visibility": "hidden", "marginTop": 16}
-    if n_clicks > 1:
-        call_to_action_style = {
-            "opacity": 1,
-            "visibility": "visible",
-            "marginTop": 16,
-            "transition": "opacity 1.0s ease",
-        }
-    # TODO: distinguish between Try Example and Submit Text - only the second one works now and it works incorrectly, it
-    #  always loads a polished
-    output_text = render_new_dataformat(df)
-    # sleep some time to show Putin loading
-    time.sleep(1)
-    return output_text, {
-        "opacity": 1,
-        "visibility": "visible",
-        "transition": "opacity 1.0s ease",
-        "min-height": 400,
-    }, call_to_action_style
+# @callback(
+#     Output(component_id="analysis_results-container", component_property="children"),
+#     Output(component_id="container-all_analysis", component_property="style"),
+#     Output(component_id="call-to-action", component_property="style"),
+#     Input(component_id="button-submit-original", component_property="n_clicks"),
+#     State(component_id="input-text_to_analyze", component_property="value"),
+#     prevent_initial_call=True,  # this prevents callback triggering at page load (before the Submit button is clicked)
+#     suppress_callback_exceptions=True,
+# )
+# def process_original(n_clicks, input_text):
+#     call_to_action_style = {"opacity": 0, "visibility": "hidden", "marginTop": 16}
+#     if n_clicks > 1:
+#         call_to_action_style = {
+#             "opacity": 1,
+#             "visibility": "visible",
+#             "marginTop": 16,
+#             "transition": "opacity 1.0s ease",
+#         }
+#
+#     # what do we want chatGPT to do?
+#     prompt = f"""
+#         Write the text delimited by triple backticks \
+#         in form of a really short russian propaganda breaking news. \
+#         Write the text in English.
+#         ```{input_text}```
+#         """
+#     output_text, output_tokens = get_completion(prompt)
+#     sentences = extract_sentences(output_text)
+#     output_text = render_new_dataformat(df)
+#     # sleep some time to show Putin loading
+#     time.sleep(1)
+#     return output_text, {
+#         "opacity": 1,
+#         "visibility": "visible",
+#         "transition": "opacity 1.0s ease",
+#         "min-height": 400,
+#     }, call_to_action_style
 
 
 @callback(
@@ -132,12 +141,12 @@ def display_mark_info(n_clicks, mark_values):
         string_with_techniques = out["classes"].values[0]
         list_of_techniques = ast.literal_eval(string_with_techniques)
 
-        # techniques_children = [style_technique(technique) for technique in list_of_techniques]
         techniques_children = []
         for i, technique in enumerate(list_of_techniques):
             techniques_children.append(style_technique(technique))
-            if i != len(list_of_techniques) - 1:
-                techniques_children.append(html.Br())
+            # TODO: why is the following not needed anymore? It changed when I wrapped the techniques in dmc.Tooltip
+            # if i != len(list_of_techniques) - 1:
+            #     techniques_children.append(html.Br())
 
         string_with_explanation = out["explain"].values[0]
         explanation = ast.literal_eval(string_with_explanation)
@@ -148,3 +157,4 @@ def display_mark_info(n_clicks, mark_values):
         ]
 
         return techniques_children, explanation_children
+
