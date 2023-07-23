bf1d2d38465e664f8ae290c08a64e9dc29ffdfc0
vsisl <vaclav.sisl@gmail.com>
2023-07-23 18:12:14 +0200

    moved celery setup to separate module

--- dash_app/app.py
+++ dash_app/app.py
@@ -7,7 +7,7 @@ To run the app (development environment only), execute the following command fro
 import dash
 from dash import Dash, html, dcc, DiskcacheManager, CeleryManager
 import dash_bootstrap_components as dbc
-import os
+from celery_app.app import celery_app
 
 # MAIN APP LAYOUT
 
@@ -89,19 +89,7 @@ navbar = dbc.Navbar(
     expand="lg",  # specify screen size at which to expand the menu bar, e.g. sm, md, lg etc.
 )
 
-# --- INITIALISING THE DASH APP
-if 'REDIS_URL' in os.environ:
-    # Use Redis & Celery if REDIS_URL set as an env variable
-    from celery import Celery
-    celery_app = Celery(__name__, broker=os.environ['REDIS_URL'], backend=os.environ['REDIS_URL'])
-    background_callback_manager = CeleryManager(celery_app)
-
-else:
-    # Diskcache for non-production apps when developing locally
-    import diskcache
-    cache = diskcache.Cache("./cache")
-    background_callback_manager = DiskcacheManager(cache)
-
+background_callback_manager = CeleryManager(celery_app)
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.BOOTSTRAP], background_callback_manager=background_callback_manager)
 app.title = "Amazing Dash app"
 
@@ -115,4 +103,4 @@ app.layout = dbc.Container(
 
 
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
